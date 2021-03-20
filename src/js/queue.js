5f6bf4e9c811562ff7abda9cc15889596227a705
Kapreio <kissduduhapp@163.com>
2021-03-21 07:47:04 +0800

    添加相关视频控制代码

--- src/js/queue.js
+++ src/js/queue.js
@@ -1,2 +1,18 @@
 import '../css/common.css'
 import '../css/queue.less'
+let videoPalyBtn = document.getElementById('videoPalyBtn')
+let queueVideo = document.getElementById('queueVideo')
+videoPalyBtn.addEventListener('click',function(){
+  videoPalyBtn.setAttribute('playing',true)
+  queueVideo.play()
+})
+queueVideo.addEventListener('ended',()=>{
+  videoPalyBtn.setAttribute('playing',false)
+})
+queueVideo.addEventListener('pause',()=>{
+  videoPalyBtn.setAttribute('playing',false)
+})
+queueVideo.addEventListener('click',()=>{
+  videoPalyBtn.setAttribute('playing',false)
+  queueVideo.pause()
+})
