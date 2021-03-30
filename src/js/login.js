f2697d7f679654944e3cf3e1ae50664f73fdc07f
Kapreio <kissduduhapp@163.com>
2021-03-30 09:35:24 +0800

    添加获取验证码接口

--- src/js/login.js
+++ src/js/login.js
@@ -1,5 +1,7 @@
 import '../css/common.css'
 import '../css/login.less'
+import sendMessage from '../common/sendMessage/message' // 引入通知小组件
+import {smsPost} from './common/api'
 // 电话输入表单DOM
 const telInput = document.getElementById('telInput')
 // 验证码输入DOM
@@ -9,7 +11,7 @@ const getCode = document.getElementById('getCode')
 // 登陆按钮DOM
 const loginBtn = document.getElementById('loginBtn')
 // 再次获取验证码间隔时长
-const countSeconds = 10
+const countSeconds = 60
 // 倒计时定时器保存变量
 let countDowmInter = null
 // 倒计时结束文本
@@ -17,29 +19,41 @@ let countDownString ='重新获取'
 
 // 获取验证码按钮绑定事件
 getCode.addEventListener('click',function(){
+  if (!/^((13[0-9])|(14[5|7])|(15([0-3]|[5-9]))|(18[0,5-9]))\d{8}$/.test(telInput.value.trim())) {
+    telInput.parentElement.setAttribute('error','true')
+    return false
+  } else{
   // 倒计时未结束不能再次获取
-  if (this.parentElement.getAttribute('countDown') === true) return false
-  // 保存倒计时时长
-  let seconds = countSeconds
-  // 已经存在倒计时先清除
-  countDowmInter && clearInterval(countDowmInter)
-  // 标记开始倒计时
-  this.parentElement.setAttribute('countDown',true)
-  // 修改倒计时文本
-  getCode.innerHTML = countDownString + `(${seconds})s`
-  // 添加定时器，动态修改倒计时文本
-  countDowmInter = setInterval(()=>{
-    seconds--
-    if (seconds === 0) {
-      // 倒计时结束，清除倒计时
-      this.parentElement.setAttribute('countDown',false)
-      clearInterval(countDowmInter)
-      countDowmInter = null
-      getCode.innerHTML = countDownString
-    } else{
-      getCode.innerHTML = countDownString + `(${seconds})s`
-    }
-  }, 1000)
+    if (this.parentElement.getAttribute('countDown') === 'true') return false
+    smsPost({name:telInput.value.trim()})
+      .then(()=>{
+        sendMessage({msg:'验证码获取成功....'})
+        // 保存倒计时时长
+        let seconds = countSeconds
+        // 已经存在倒计时先清除
+        countDowmInter && clearInterval(countDowmInter)
+        // 标记开始倒计时
+        this.parentElement.setAttribute('countDown','true')
+        // 修改倒计时文本
+        getCode.innerHTML = countDownString + `(${seconds})s`
+        // 添加定时器，动态修改倒计时文本
+        countDowmInter = setInterval(()=>{
+          seconds--
+          if (seconds === 0) {
+            // 倒计时结束，清除倒计时
+            this.parentElement.setAttribute('countDown','false')
+            clearInterval(countDowmInter)
+            countDowmInter = null
+            getCode.innerHTML = countDownString
+          } else{
+            getCode.innerHTML = countDownString + `(${seconds})s`
+          }
+        }, 1000)
+      })
+      .catch(err=>{
+        sendMessage({msg:err})
+      })
+  }
 })
 // 给电话输入表单绑定事件
 telInput.addEventListener('input',loginBtnEnabel)
