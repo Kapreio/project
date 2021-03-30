07b22bd78a8c3e0df7a315938f0fb64483b780e1
Kapreio <kissduduhapp@163.com>
2021-03-30 08:11:30 +0800

    余额使用接口获取

--- src/js/balance.js
+++ src/js/balance.js
@@ -2,6 +2,7 @@ import '../css/common.css'
 import '../css/balance.less'
 import sendMessage from '../common/sendMessage/message'
 import qs from './common/qs'
+import {getBalance} from './common/api'
 
 const balance = document.getElementById('balance')
 const chargeLog = document.getElementById('chargeLog')
@@ -10,26 +11,30 @@ const chargeAfter =  document.getElementById('chargeAfter')
 const afterBalance =  document.getElementById('afterBalance')
 const chargeNow = document.getElementById('chargeNow')
 let query = qs.urlParse()
-balance.innerHTML = query.balance
 chargeLog.href = chargeLog.href +  `?id=${query.id}`
 
-chargeInput.addEventListener('input',function(){
-  if(this.value){
-    afterBalance.innerHTML = query.balance - 0 + parseInt(this.value)
-    chargeAfter.setAttribute('show',true)
-  } else {
-    chargeAfter.setAttribute('show',false)
-  }
-})
-chargeNow.addEventListener('click',function(){
-  if(chargeInput.value >= 10) {
-    query.balance =  query.balance - 0 + parseInt(chargeInput.value)
-    balance.innerHTML = query.balance
-    chargeInput.value = ''
-    chargeAfter.setAttribute('show',false)
-    sendMessage({msg:'充值金额成功！'})
-  } else {
-    chargeInput.focus()
-    sendMessage({msg:'请输入正确的充值金额！！！'})
-  }
-})
+getBalance()
+  .then(data=>{
+    balance.innerHTML = data.toFixed(2)
+    query.balance = data
+    chargeInput.addEventListener('input',function(){
+      if(this.value){
+        afterBalance.innerHTML = query.balance - 0 + parseInt(this.value)
+        chargeAfter.setAttribute('show',true)
+      } else {
+        chargeAfter.setAttribute('show',false)
+      }
+    })
+    chargeNow.addEventListener('click',function(){
+      if(chargeInput.value >= 10) {
+        query.balance =  query.balance - 0 + parseInt(chargeInput.value)
+        balance.innerHTML = query.balance
+        chargeInput.value = ''
+        chargeAfter.setAttribute('show',false)
+        sendMessage({msg:'充值金额成功！'})
+      } else {
+        chargeInput.focus()
+        sendMessage({msg:'请输入正确的充值金额！！！'})
+      }
+    })
+  })
