6bc7c31ea5d747694cf224c01853ab8c4d064c8a
Kapreio <kissduduhapp@163.com>
2021-04-01 23:41:29 +0800

    完成登录状态验证功能

--- src/common/loginValidate/loginValidate.js
+++ src/common/loginValidate/loginValidate.js
@@ -1,1 +1,10 @@
 import './loginValidate.less'
+import {getCookie,setCookie} from '../../js/common/utils'
+window.setCookie = setCookie
+if (!getCookie('token')) {
+  alert('当前页面需要登录后才能访问，请登录！') 
+  document.body.innerHTML = '<div>请登录！</div>'
+  location.href = `login.html?href=${location.href}`
+} else {
+  document.body.setAttribute('login',new Date().getTime())
+}
