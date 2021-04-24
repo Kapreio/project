635cb76be020ece9388c94bcd010c96645c25338
Kapreio <kissduduhapp@163.com>
2021-04-24 20:59:36 +0800

    update

--- src/js/mine.js
+++ src/js/mine.js
@@ -1,27 +1,24 @@
 import '../css/common.css'
 import '../css/mine.less'
-import '../img/my_img.png'
-import {getUseInfo} from './common/api'
-import qs from './common/qs'
-import {isLogined} from '../common/loginValidate/loginValidate'
+import '../img/my_img.png' // 导入头像默认图片，在mine.html中使用
+import {getUseInfo} from './common/api' // 导入获取用户信息方法
+import {isLogined} from '../common/loginValidate/loginValidate' // 导入登录判断方法
 
+// 判断是否登录
 isLogined(loginedOperation)
-function loginedOperation () {
 
+// 将登录成功后的操作写在loginedOperation函数中
+function loginedOperation () {
+  // 获取显示手机号码DOM
   const telNum = document.getElementById('telNum')
-  const toBalance = document.getElementById('toBalance')
-  const toWalletLog = document.getElementById('toWalletLog')
-  const toTelBind = document.getElementById('toTelBind')
+  // 获取用户头像DOM
   const hearder = document.getElementById('hearder')
   getUseInfo()
     .then(data=>{
-      let queryStr = qs.stringify({id:data.id})
+      // 更新手机号码
       telNum.innerHTML = data.username
-      toBalance.href = toBalance.href + `?${queryStr}&balance=${data.balance}`
-      toWalletLog.href = toWalletLog.href + `?${queryStr}&cost=true`
-      toTelBind.href = toTelBind.href + `?${queryStr}`
+      // 更新用户头像
       hearder.src = data.header
       hearder.alt = data.nickname
-      console.log(data)
     })
 }
